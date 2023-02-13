29e240dd5d806127b0c7d4b96b878dc137cba126
ValeriaKul <lera.dribnyuk@mail.ru>
2023-02-13 18:41:52 +0200

    changed spaces

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,33 +1,21 @@
-import React from 'react'
-import './style.modules.css'
+import React from "react";
+import "./style.modules.css";
 
-export default function AddProduct({createProduct}) {
-
-    const onSubmit = (e) => {
+export default function AddProduct({ createProduct }) {
+  const onSubmit = (e) => {
     e.preventDefault();
-    const {title, price} = e.target;
+    const { title, price } = e.target;
     createProduct(title.value, +price.value);
-    title.value = '';
-    price.value = '';
-  }
+    title.value = "";
+    price.value = "";
+  };
   return (
     <>
-    <form 
-        className='formAdd'
-        onSubmit={onSubmit} >
-            <input 
-                type="text" 
-                placeholder='Title' 
-                name='title'/>
-            <input 
-                type="number" 
-                placeholder='Price' 
-                name='price'/>
-            <button
-                className='btnAdd'>
-                Add
-            </button>
-    </form>
+      <form className="formAdd" onSubmit={onSubmit}>
+        <input type="text" placeholder="Title" name="title" />
+        <input type="number" placeholder="Price" name="price" />
+        <button className="btnAdd">Add</button>
+      </form>
     </>
-  )
+  );
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import s from './style.module.css'
+import React from "react";
+import s from "./style.module.css";
 
-export default function Products({id,title,price, deleteProduct, changePrice}) {
-
+export default function Products({
+  id,
+  title,
+  price,
+  deleteProduct,
+  changePrice,
+}) {
   return (
     <div className={s.card}>
-        <p 
-          className={[price === 0 ? s.free  : s.title]}
-         
-          >{title}</p>
-        <p>$ {price || 'Free'}</p>  
-        <button
-          className={s.btnDelete}
-          onClick={() => deleteProduct(id)}>Delete
-        </button>
-        <button
-          className={s.btnAdd}
-          onClick={()=> changePrice(id, 10)}>+10
-        </button>
-        {
-          price !== 0
-          ? <button
-          onClick={()=> changePrice(id, -10)}>-10
-          </button>
-          : '' 
-        }
-
+      <p className={[price === 0 ? s.free : s.title]}>{title}</p>
+      <p>$ {price || "Free"}</p>
+      <button className={s.btnDelete} onClick={() => deleteProduct(id)}>
+        Delete
+      </button>
+      <button className={s.btnAdd} onClick={() => changePrice(id, 10)}>
+        +10
+      </button>
+      {price !== 0 ? (
+        <button onClick={() => changePrice(id, -10)}>-10</button>
+      ) : (
+        ""
+      )}
     </div>
-    
-  )
+  );
 }
